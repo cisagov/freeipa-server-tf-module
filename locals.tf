52380dff8877939392ff825801ad03193e06cfb8
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-08 15:55:57 -0400

    Add all ports that FreeIPA supports

--- locals.tf
+++ locals.tf
@@ -1,14 +1,25 @@
 locals {
   # The TCP ports that the IPA servers should listen on
   ipa_tcp_ports = [
-    88,  # Kerberos
+    53,  # DNS
+    80,  # HTTP
+    88,  # kinit
+    443, # HTTPS
     464, # kpasswd
     389, # LDAP
+    636, # LDAPS
   ]
 
   # The UDP ports that the IPA servers should listen on
   ipa_udp_ports = [
-    88,  # Kerberos
+    53,  # DNS
+    88,  # kinit
+    123, # NTP
     464, # kpasswd
+  ]
+
+  tcp_and_udp = [
+    "tcp",
+    "udp"
   ]
 }
