0e57c8064ea5344fe9a40c086a513727116ba64d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-09 15:59:02 -0400

    Add ssh.  Remove now-unused variable.

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
   # The TCP ports that the IPA servers should listen on
   ipa_tcp_ports = [
+    22,  # SSH
     53,  # DNS
     80,  # HTTP
     88,  # kinit
@@ -16,10 +17,5 @@ locals {
     88,  # kinit
     123, # NTP
     464, # kpasswd
-  ]
-
-  tcp_and_udp = [
-    "tcp",
-    "udp"
   ]
 }
