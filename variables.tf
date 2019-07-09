e9be92f632a90ae2546291b6bac567932bb7076b
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-09 16:00:12 -0400

    Remove zone name variable and add variables needed to configure FreeIPA

--- variables.tf
+++ variables.tf
@@ -4,6 +4,26 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
+variable "admin_pw" {
+  description = "The admin password for the IPA server's Kerberos admin role"
+}
+
+variable "directory_service_pw" {
+  description = "The password for the IPA server's directory service"
+}
+
+variable "domain" {
+  description = "The domain for the IPA server (e.g. example.com)"
+}
+
+variable "hostname" {
+  description = "The hostname of the IPA server (e.g. ipa.example.com)"
+}
+
+variable "realm" {
+  description = "The realm for the IPA server (e.g. EXAMPLE.COM)"
+}
+
 variable "subnet_id" {
   description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)"
 }
@@ -13,15 +33,17 @@ variable "trusted_cidr_blocks" {
   description = "A list of the CIDR blocks that are allowed to access the IPA servers (e.g. [\"10.10.0.0/16\", \"10.11.0.0/16\"])"
 }
 
-variable "zone_name" {
-  description = "The name corresponding to the Route53 zone where the kerberos-related DNS records should be created (e.g. local or example.com)"
-}
-
 # ------------------------------------------------------------------------------
 # Optional parameters
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
+
+variable "associate_public_ip_address" {
+  type        = bool
+  description = "Whether or not to associate a public IP address with the IPA server"
+  default     = false
+}
 
 variable "aws_instance_type" {
   description = "The AWS instance type to deploy (e.g. t3.micro)"
