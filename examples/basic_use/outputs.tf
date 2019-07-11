049ab8aafa208c598461d574ec352ee58ee4997c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-11 14:35:22 -0400

    Rename some output variables

--- examples/basic_use/outputs.tf
+++ examples/basic_use/outputs.tf
@@ -1,39 +1,39 @@
-output "master_id" {
-  value       = module.ipa.master_id
+output "id" {
+  value       = module.ipa.id
   description = "The EC2 instance ID corresponding to the IPA master"
 }
 
-output "master_arn" {
-  value       = module.ipa.master_arn
+output "arn" {
+  value       = module.ipa.arn
   description = "The EC2 instance ARN corresponding to the IPA master"
 }
 
-output "master_availability_zone" {
-  value       = module.ipa.master_availability_zone
+output "availability_zone" {
+  value       = module.ipa.availability_zone
   description = "The AZ where the IPA master instance is deployed"
 }
 
-output "master_private_ip" {
-  value       = module.ipa.master_private_ip
+output "private_ip" {
+  value       = module.ipa.private_ip
   description = "The private IP of the IPA master instance"
 }
 
-output "master_public_ip" {
-  value       = module.ipa.master_public_ip
+output "public_ip" {
+  value       = module.ipa.public_ip
   description = "The public IP of the IPA master instance"
 }
 
-output "master_subnet_id" {
-  value       = module.ipa.master_subnet_id
+output "subnet_id" {
+  value       = module.ipa.subnet_id
   description = "The ID of the subnet where the IPA master instance is deployed"
 }
 
-output "master_security_group_id" {
-  value       = module.ipa.master_security_group_id
+output "security_group_id" {
+  value       = module.ipa.security_group_id
   description = "The ID of the IPA server security group"
 }
 
-output "master_security_group_arn" {
-  value       = module.ipa.master_security_group_arn
+output "security_group_arn" {
+  value       = module.ipa.security_group_arn
   description = "The ARN of the IPA server security group"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,39 @@
-output "master_id" {
+output "id" {
   value       = aws_instance.ipa_master.id
   description = "The EC2 instance ID corresponding to the IPA master"
 }
 
-output "master_arn" {
+output "arn" {
   value       = aws_instance.ipa_master.arn
   description = "The EC2 instance ARN corresponding to the IPA master"
 }
 
-output "master_availability_zone" {
+output "availability_zone" {
   value       = aws_instance.ipa_master.availability_zone
   description = "The AZ where the IPA master instance is deployed"
 }
 
-output "master_private_ip" {
+output "private_ip" {
   value       = aws_instance.ipa_master.private_ip
   description = "The private IP of the IPA master instance"
 }
 
-output "master_public_ip" {
+output "public_ip" {
   value       = aws_instance.ipa_master.public_ip
   description = "The public IP of the IPA master instance"
 }
 
-output "master_subnet_id" {
+output "subnet_id" {
   value       = aws_instance.ipa_master.subnet_id
   description = "The ID of the subnet where the IPA master instance is deployed"
 }
 
-output "master_security_group_id" {
+output "security_group_id" {
   value       = aws_security_group.ipa_servers.id
   description = "The ID of the IPA server security group"
 }
 
-output "master_security_group_arn" {
+output "security_group_arn" {
   value       = aws_security_group.ipa_servers.arn
   description = "The ARN of the IPA server security group"
 }
