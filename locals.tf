3f6db4849190d1d8b910d96edb674718c8b26bae
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-11 23:17:54 -0400

    Remove DNS and NTP ports from list of open ports

--- locals.tf
+++ locals.tf
@@ -2,7 +2,6 @@ locals {
   # The TCP ports that the IPA servers should listen on
   ipa_tcp_ports = [
     22,  # SSH
-    53,  # DNS
     80,  # HTTP
     88,  # kinit
     443, # HTTPS
@@ -13,9 +12,7 @@ locals {
 
   # The UDP ports that the IPA servers should listen on
   ipa_udp_ports = [
-    53,  # DNS
     88,  # kinit
-    123, # NTP
     464, # kpasswd
   ]
 
