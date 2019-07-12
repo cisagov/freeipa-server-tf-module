713099f27168596ba90e9ae7208952e1bb169f45
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-07-12 12:15:46 -0400

    Improve FreeIPA server install script
    Wait until the instance's private IP address has a non-Amazon PTR
    record before installing FreeIPA.  The installation will fail if the
    PTR record is not correct, and it can take some time for the PTR
    record resource to be created, so it makes sense to wait until it
    becomes available.

--- scripts/setup_freeipa.sh
+++ scripts/setup_freeipa.sh
@@ -13,11 +13,30 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-ip_address=$(ip --family inet address show dev eth0 | \
-                 grep inet | \
-                 sed "s/^ *//" | \
-                 cut --delimiter=' ' --fields=2 | \
-                 cut --delimiter='/' --fields=1)
+# Get the IP address corresponding to an interface
+function get_ip {
+    ip --family inet address show dev "$1" | \
+        grep inet | \
+        sed "s/^ *//" | \
+        cut --delimiter=' ' --fields=2 | \
+        cut --delimiter='/' --fields=1
+}
+
+# Get the PTR record corresponding to an IP
+function get_ptr {
+    dig +noall +ans -x "$1" | sed "s/.*PTR[[:space:]]*\(.*\)/\1/"
+}
+
+ip_address=$(get_ip eth0)
+
+# Wait until the IP address has a non-Amazon PTR record before
+# proceeding
+ptr=$(get_ptr "$ip_address")
+while grep amazon <<< "$ptr"
+do
+    sleep 30
+    ptr=$(get_ptr "$ip_address")
+done
 
 # There are several items below that look like shell variables but are
 # actually replaced by the Terraform templating engine.  Hence we can
