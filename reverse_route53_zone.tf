7b65b224f0637029c5772b9f1dcecfdb32a0dcf8
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-08-13 17:23:06 -0400

    Allow the master and each replica to use a different private reverse zone
    The master and each replica may be in different subnets, and hence in
    different /24 blocks, so this is necessary.

--- reverse_route53_zone.tf
+++ reverse_route53_zone.tf
@@ -2,7 +2,7 @@
 # Create some DNS records.
 #-------------------------------------------------------------------------------
 resource "aws_route53_record" "master_private_PTR" {
-  zone_id = var.private_reverse_zone_id
+  zone_id = var.master_private_reverse_zone_id
   name = format(
     "%s.%s.%s.%s.in-addr.arpa.",
     element(split(".", aws_instance.ipa_master.private_ip), 3),
@@ -21,7 +21,7 @@ resource "aws_route53_record" "master_private_PTR" {
 resource "aws_route53_record" "replica_private_PTR" {
   count = length(data.aws_subnet.replica_subnets)
 
-  zone_id = var.private_reverse_zone_id
+  zone_id = var.replica_private_reverse_zone_ids[count.index]
   name = format(
     "%s.%s.%s.%s.in-addr.arpa.",
     element(split(".", aws_instance.ipa_replicas[count.index].private_ip), 3),
