f3cef8cbf750fb957de3c4ec164b5367c203b63d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-11 15:40:47 -0400

    Remove unnecessary code from instance_role.tf

--- instance_role.tf
+++ instance_role.tf
@@ -19,30 +19,6 @@ resource "aws_iam_role" "ipa" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
 }
 
-# # IAM policy document that that allows read access to the certboto
-# # certificate data.  This will be applied to the role we are creating.
-# data "aws_iam_policy_document" "s3_doc" {
-#   statement {
-#     effect = "Allow"
-
-#     actions = [
-#       "s3:GetObject",
-#     ]
-
-#     resources = [
-#       "arn:aws:s3:::cert-bucket-name/live/${var.hostname}/*",
-#     ]
-#   }
-# }
-
-# # The S3 policy for our role
-# resource "aws_iam_role_policy" "s3_policy" {
-#   provider = aws.certs
-
-#   role   = aws_iam_role.ipa.id
-#   policy = data.aws_iam_policy_document.s3_doc.json
-# }
-
 # IAM policy document that allows assumption of a delegated role
 data "aws_iam_policy_document" "assume_delegated_role_policy_doc" {
   statement {
