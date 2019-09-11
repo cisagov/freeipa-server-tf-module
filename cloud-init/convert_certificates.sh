9e6a595b06801c675c3ca094bdd4508f4c115d33
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-11 15:29:28 -0400

    Add code to download some Let's Encrypt root certs
    FreeIPA insists on having these.

--- cloud-init/convert_certificates.sh
+++ cloud-init/convert_certificates.sh
@@ -4,6 +4,14 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# Grab some root certificates for Let's Encrypt that we will need when
+# we install FreeIPA
+curl -o /etc/ipa/isrgrootx1.pem \
+     https://letsencrypt.org/certs/isrgrootx1.pem.txt
+curl -o /etc/ipa/letsencryptauthorityx3.pem \
+     https://letsencrypt.org/certs/letsencryptauthorityx3.pem.txt
+
+# Convert the PEMs to PKCS#12 format
 openssl pkcs12 -export \
         -out /etc/ipa/cert.p12 \
         -in /etc/ipa/fullchain.pem \
