f6fddad00611c8b28d9023afaadfbbc4766e460d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-10 23:13:11 -0400

    Rename the scripts directory to cloud-init

--- cloud_init.tf
+++ cloud_init.tf
@@ -8,7 +8,7 @@ data "template_cloudinit_config" "cloud_init_tasks" {
     content_type = "text/cloud-config"
     filename     = "download-certificates.yml"
     content = templatefile(
-      "${path.module}/scripts/download-certificates.tpl.yml", {
+      "${path.module}/cloud-init/download-certificates.tpl.yml", {
         cert_bucket_name   = "cool-certificates"
         cert_read_role_arn = var.cert_read_role_arn
         server_fqdn        = var.hostname
@@ -18,13 +18,13 @@ data "template_cloudinit_config" "cloud_init_tasks" {
   part {
     content_type = "text/x-shellscript"
     content = templatefile(
-    "${path.module}/scripts/convert_certificates.sh", {})
+    "${path.module}/cloud-init/convert_certificates.sh", {})
   }
 
   part {
     content_type = "text/x-shellscript"
     content = templatefile(
-      "${path.module}/scripts/setup_freeipa.sh", {
+      "${path.module}/cloud-init/setup_freeipa.sh", {
         directory_service_pw = var.directory_service_pw
         admin_pw             = var.admin_pw
         domain               = var.domain
