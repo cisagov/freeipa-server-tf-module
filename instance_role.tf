68ca93a2b25f0c4b2bc386ba797d3a9d99a4a002
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-12 10:44:19 -0400

    Change the names of the instance role and profile
    It is possible that there could be more than one master in a given
    workspace, and in that case the hostname would be unique whereas the
    workspace may not be.

--- instance_role.tf
+++ instance_role.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "assume_role_doc" {
 # The S3 certificate access role to be used by the IPA master EC2
 # instance
 resource "aws_iam_role" "ipa" {
-  name               = "ipa_master_instance_role_${terraform.workspace}"
+  name               = "ipa_master_instance_role_${var.hostname}"
   assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
 }
 
@@ -41,6 +41,6 @@ resource "aws_iam_role_policy" "assume_delegated_role_policy" {
 
 # The instance profile to be used by the IPA master EC2 instance.
 resource "aws_iam_instance_profile" "ipa" {
-  name = "ipa_master_instance_profile_${terraform.workspace}"
+  name = "ipa_master_instance_profile_${var.hostname}"
   role = aws_iam_role.ipa.name
 }
