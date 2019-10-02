1c3cc03c66c6c02aaa7380575d9dba03996677d6
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-10-01 22:51:30 -0400

    Add client SG as output

--- examples/basic_usage/outputs.tf
+++ examples/basic_usage/outputs.tf
@@ -1,3 +1,8 @@
+output "ipa_client_security_group_id" {
+  value       = module.ipa_master.client_security_group_id
+  description = "The ID corresponding to the IPA client security group"
+}
+
 output "ipa_server_security_group_id" {
   value       = module.ipa_master.server_security_group_id
   description = "The ID corresponding to the IPA server security group"
