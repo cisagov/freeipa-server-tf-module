3e00b16e439f0acb473b6a2ac46746e0a4dc3664
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-24 15:30:55 -0500

    Alphabetize

--- examples/basic_usage/backend.tf
+++ examples/basic_usage/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "cisa-cool-terraform-state"
-    encrypt        = true
     dynamodb_table = "terraform-state-lock"
+    encrypt        = true
     key            = "freeipa-master-tf-module/examples/basic_usage/terraform.tfstate"
     profile        = "cool-terraform-backend"
     region         = "us-east-1"
