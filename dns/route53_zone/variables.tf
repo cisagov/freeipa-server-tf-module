7f16a7bc5673c323c353498b501932551c9f8584
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-13 16:30:41 -0400

    Add types for all variables

--- dns/route53_zone/variables.tf
+++ dns/route53_zone/variables.tf
@@ -5,18 +5,22 @@
 # ------------------------------------------------------------------------------
 
 variable "domain" {
+  type        = string
   description = "The domain for the IPA master (e.g. example.com)"
 }
 
 variable "hostname" {
+  type        = string
   description = "The hostname of the IPA master (e.g. ipa.example.com)"
 }
 
 variable "ip" {
+  type        = string
   description = "The IP of the IPA master (e.g. 10.11.1.5)"
 }
 
 variable "zone_id" {
+  type        = string
   description = "The zone ID corresponding to the Route53 zone where the Kerberos-related DNS records should be created (e.g. ZKX36JXQ8W82L)"
 }
 
@@ -28,11 +32,13 @@ variable "zone_id" {
 # ------------------------------------------------------------------------------
 
 variable "do_it" {
+  type        = bool
   description = "If false then no resources are created.  This is a workaround until Terraform modules support the count and/or for_each directives."
   default     = true
 }
 
 variable "ttl" {
+  type        = number
   description = "The TTL value to use for Route53 DNS records (e.g. 86400).  A smaller value may be useful when the DNS records are changing often, for example when testing."
   default     = 86400
 }
