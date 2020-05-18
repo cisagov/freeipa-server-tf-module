55f0dd9f8ada2f1c1a3226b08824d8cfd48f75dd
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-18 09:05:45 -0400

    Tweak comment

--- providers.tf
+++ providers.tf
@@ -1,8 +1,9 @@
+# The "default" provider that is used to do everything _but_ create
+# public DNS entries
+provider "aws" {
+}
+
 # The provider that is used to create public DNS entries
 provider "aws" {
   alias = "public_dns"
-}
-
-# The "default" provider that is used to do everything else
-provider "aws" {
 }
