ac3b0e589243afee66870fcef15e4d46cf3b0f42
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-28 12:55:08 -0400

    Remove the certificate retrieval and certificate massaging scripts
    These are no longer required since we are allowing FreeIPA to generate
    its own certificates.

--- cloud_init.tf
+++ cloud_init.tf
@@ -7,24 +7,6 @@ data "template_cloudinit_config" "cloud_init_tasks" {
   part {
     content_type = "text/x-shellscript"
     content = templatefile(
-      "${path.module}/cloud-init/download-certificates.py", {
-        cert_bucket_name   = var.cert_bucket_name
-        cert_read_role_arn = var.cert_read_role_arn
-        server_fqdn        = var.hostname
-    })
-  }
-
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile(
-      "${path.module}/cloud-init/convert_certificates.sh", {
-        cert_pw = var.cert_pw
-    })
-  }
-
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile(
       "${path.module}/cloud-init/setup_freeipa.sh", {
         admin_pw             = var.admin_pw
         cert_pw              = var.cert_pw
