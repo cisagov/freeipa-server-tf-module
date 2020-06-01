f60df766e2dbd7e99b670806ac45f958d4457e30
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-06-01 11:49:36 -0400

    Add NTP and DNS ports

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
   # The TCP ports that the IPA servers should listen on
   ipa_tcp_ports = [
+    53,  # DNS
     80,  # HTTP
     88,  # kinit
     443, # HTTPS
@@ -11,7 +12,9 @@ locals {
 
   # The UDP ports that the IPA servers should listen on
   ipa_udp_ports = [
+    53,  # DNS
     88,  # kinit
+    123, # NTP
     464, # kpasswd
   ]
 }
