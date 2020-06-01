12acede69c1d88ef061fa71deeb19f62f0516f5c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-06-01 14:04:41 -0400

    Simplify DNS
    This is possible now that public DNS records are no longer supported.

--- dns.tf
+++ dns.tf
@@ -1,14 +1,29 @@
 #-------------------------------------------------------------------------------
-# Configure the DNS module
+# Create A and PTR records for the IPA server
 #-------------------------------------------------------------------------------
-module "dns" {
-  source = "./dns"
+resource "aws_route53_record" "server_A" {
+  zone_id = var.zone_id
+  name    = var.hostname
+  type    = "A"
+  ttl     = var.ttl
+  records = [
+    aws_instance.ipa.private_ip,
+  ]
+}
 
-  domain          = var.domain
-  hostname        = var.hostname
-  ip              = aws_instance.ipa.private_ip
-  reverse_zone_id = var.reverse_zone_id
-  tags            = var.tags
-  ttl             = var.ttl
-  zone_id         = var.zone_id
+resource "aws_route53_record" "ptr" {
+  zone_id = var.reverse_zone_id
+  name = format(
+    "%s.%s.%s.%s.in-addr.arpa.",
+    element(split(".", aws_instance.ipa.private_ip), 3),
+    element(split(".", aws_instance.ipa.private_ip), 2),
+    element(split(".", aws_instance.ipa.private_ip), 1),
+    element(split(".", aws_instance.ipa.private_ip), 0),
+  )
+
+  type = "PTR"
+  ttl  = var.ttl
+  records = [
+    var.hostname
+  ]
 }
