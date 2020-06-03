a3b8d5b5efe63d5381e610e92978e6f9e4112df1
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-06-03 11:22:59 -0400

    Update example code for removal of DNS TF code

--- examples/basic_usage/main.tf
+++ examples/basic_usage/main.tf
@@ -38,29 +38,6 @@ resource "aws_route" "route_external_traffic_through_internet_gateway" {
   gateway_id             = aws_internet_gateway.the_igw.id
 }
 
-
-#-------------------------------------------------------------------------------
-# Create a private Route53 zone.
-#-------------------------------------------------------------------------------
-resource "aws_route53_zone" "private_zone" {
-  name = "cyber.dhs.gov"
-
-  vpc {
-    vpc_id = aws_vpc.the_vpc.id
-  }
-}
-
-#-------------------------------------------------------------------------------
-# Create a private Route53 reverse zone.
-#-------------------------------------------------------------------------------
-resource "aws_route53_zone" "private_reverse_zone" {
-  name = "48.99.10.in-addr.arpa"
-
-  vpc {
-    vpc_id = aws_vpc.the_vpc.id
-  }
-}
-
 #-------------------------------------------------------------------------------
 # Configure the server module.
 #-------------------------------------------------------------------------------
@@ -70,8 +47,6 @@ module "ipa" {
   ami_owner_account_id = "207871073513" # The COOL Images account
   domain               = "cal23.cyber.dhs.gov"
   hostname             = "ipa.cal23.cyber.dhs.gov"
-  reverse_zone_id      = aws_route53_zone.private_reverse_zone.zone_id
-  zone_id              = aws_route53_zone.private_zone.zone_id
   realm                = "CAL23.CYBER.DHS.GOV"
   subnet_id            = aws_subnet.subnet.id
   tags = {
@@ -81,5 +56,4 @@ module "ipa" {
     "108.31.3.53/32",
     "64.69.57.0/24",
   ]
-  ttl = 60
 }
